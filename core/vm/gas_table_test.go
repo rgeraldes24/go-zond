0673d3f770145b1b0ee5cbe179114acdce08442e
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-08-26 12:14:47 +0400

    core/vm/gas_table_test.go: remove old test

--- core/vm/gas_table_test.go
+++ core/vm/gas_table_test.go
@@ -24,7 +24,6 @@ import (
 
 	"github.com/theQRL/go-zond/common"
 	"github.com/theQRL/go-zond/common/hexutil"
-	"github.com/theQRL/go-zond/common/math"
 	"github.com/theQRL/go-zond/core/rawdb"
 	"github.com/theQRL/go-zond/core/state"
 	"github.com/theQRL/go-zond/core/types"
@@ -47,64 +46,6 @@ func TestMemoryGasCost(t *testing.T) {
 		}
 		if v != tt.cost {
 			t.Errorf("test %d: gas cost mismatch: have %v, want %v", i, v, tt.cost)
-		}
-	}
-}
-
-var eip3529Tests = []struct {
-	original byte
-	gaspool  uint64
-	input    string
-	used     uint64
-	refund   uint64
-	failure  error
-}{
-	{0, math.MaxUint64, "0x60006000556000600055", 2312, 0, nil},                // 0 -> 0 -> 0
-	{0, math.MaxUint64, "0x60006000556001600055", 22212, 0, nil},               // 0 -> 0 -> 1
-	{0, math.MaxUint64, "0x60016000556000600055", 22212, 19900, nil},           // 0 -> 1 -> 0
-	{0, math.MaxUint64, "0x60016000556002600055", 22212, 0, nil},               // 0 -> 1 -> 2
-	{0, math.MaxUint64, "0x60016000556001600055", 22212, 0, nil},               // 0 -> 1 -> 1
-	{1, math.MaxUint64, "0x60006000556000600055", 5112, 4800, nil},             // 1 -> 0 -> 0
-	{1, math.MaxUint64, "0x60006000556001600055", 5112, 2800, nil},             // 1 -> 0 -> 1
-	{1, math.MaxUint64, "0x60006000556002600055", 5112, 0, nil},                // 1 -> 0 -> 2
-	{1, math.MaxUint64, "0x60026000556000600055", 5112, 4800, nil},             // 1 -> 2 -> 0
-	{1, math.MaxUint64, "0x60026000556003600055", 5112, 0, nil},                // 1 -> 2 -> 3
-	{1, math.MaxUint64, "0x60026000556001600055", 5112, 2800, nil},             // 1 -> 2 -> 1
-	{1, math.MaxUint64, "0x60026000556002600055", 5112, 0, nil},                // 1 -> 2 -> 2
-	{1, math.MaxUint64, "0x60016000556000600055", 5112, 4800, nil},             // 1 -> 1 -> 0
-	{1, math.MaxUint64, "0x60016000556002600055", 5112, 0, nil},                // 1 -> 1 -> 2
-	{1, math.MaxUint64, "0x60016000556001600055", 2312, 0, nil},                // 1 -> 1 -> 1
-	{0, math.MaxUint64, "0x600160005560006000556001600055", 42218, 19900, nil}, // 0 -> 1 -> 0 -> 1
-	{1, math.MaxUint64, "0x600060005560016000556000600055", 8018, 7600, nil},   // 1 -> 0 -> 1 -> 0
-	{1, 2306, "0x6001600055", 2306, 0, ErrOutOfGas},                            // 1 -> 1 (2300 sentry + 2xPUSH)
-	{1, 2307, "0x6001600055", 2206, 0, nil},
-}
-
-func TestEIP3529(t *testing.T) {
-	for i, tt := range eip3529Tests {
-		address := common.BytesToAddress([]byte("contract"))
-		statedb, _ := state.New(types.EmptyRootHash, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
-		statedb.AddAddressToAccessList(address)
-		statedb.CreateAccount(address)
-		statedb.SetCode(address, hexutil.MustDecode(tt.input))
-		statedb.SetState(address, common.Hash{}, common.BytesToHash([]byte{tt.original}))
-		statedb.Finalise(true) // Push the state into the "original" slot
-
-		vmctx := BlockContext{
-			CanTransfer: func(StateDB, common.Address, *big.Int) bool { return true },
-			Transfer:    func(StateDB, common.Address, common.Address, *big.Int) {},
-		}
-		vmenv := NewEVM(vmctx, TxContext{}, statedb, params.AllBeaconProtocolChanges, Config{ExtraEips: []int{}})
-
-		_, gas, err := vmenv.Call(AccountRef(common.Address{}), address, nil, tt.gaspool, new(big.Int))
-		if err != tt.failure {
-			t.Errorf("test %d: failure mismatch: have %v, want %v", i, err, tt.failure)
-		}
-		if used := tt.gaspool - gas; used != tt.used {
-			t.Errorf("test %d: gas used mismatch: have %v, want %v", i, used, tt.used)
-		}
-		if refund := vmenv.StateDB.GetRefund(); refund != tt.refund {
-			t.Errorf("test %d: gas refund mismatch: have %v, want %v", i, refund, tt.refund)
 		}
 	}
 }
