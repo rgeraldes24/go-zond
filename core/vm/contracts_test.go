8b697d85d8a401b4bbb33c5480d2c2ac93923838
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-09-30 11:52:23 +0400

    cmd/evm/testdata: remove old field

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -222,7 +222,7 @@ func BenchmarkPrecompiledBn256ScalarMul(b *testing.B) { benchJson("bn256ScalarMu
 func TestPrecompiledBn256Pairing(t *testing.T)      { testJson("bn256Pairing", "08", t) }
 func BenchmarkPrecompiledBn256Pairing(b *testing.B) { benchJson("bn256Pairing", "08", b) }
 
-func TestPrecompiledDepositroot(t *testing.T) { testJson("depositRoot", "01", t) }
+func TestPrecompiledDepositroot(t *testing.T) { testJson("depositroot", "01", t) }
 
 func testJson(name, addr string, t *testing.T) {
 	tests, err := loadJson(name)
