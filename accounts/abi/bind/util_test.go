a209776966ea0eba311fee1ff567d9d6187e0cca
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2024-11-15 22:59:31 +0400

    accounts/abi/bind/util_test.go: review file

--- accounts/abi/bind/util_test.go
+++ accounts/abi/bind/util_test.go
@@ -33,9 +33,7 @@ import (
 
 var testKey, _ = pqcrypto.HexToDilithium("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 
-var wantedAddr1, _ = common.NewAddressFromString("Zfe66B8AED6e4fb3e12d0B65f61ef246c4d0CfFFA")
-var wantedAddr2, _ = common.NewAddressFromString("Zfe66B8AED6e4fb3e12d0B65f61ef246c4d0CfFFA")
-
+var wantedAddr, _ = common.NewAddressFromString("0xfe66B8AED6e4fb3e12d0B65f61ef246c4d0CfFFA")
 var waitDeployedTests = map[string]struct {
 	code        string
 	gas         uint64
@@ -45,13 +43,13 @@ var waitDeployedTests = map[string]struct {
 	"successful deploy": {
 		code:        `6060604052600a8060106000396000f360606040526008565b00`,
 		gas:         3000000,
-		wantAddress: wantedAddr1,
+		wantAddress: wantedAddr,
 	},
 	"empty code": {
 		code:        ``,
 		gas:         300000,
 		wantErr:     bind.ErrNoCodeAfterDeploy,
-		wantAddress: wantedAddr2,
+		wantAddress: wantedAddr,
 	},
 }
 
